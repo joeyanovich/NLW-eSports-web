45048cbc20b06eab6baf7683cc42a15fc0986ffd
joeyanovich <joedison.dias@gmail.com>
2022-10-14 23:55:55 -0300

    Home page finished

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,9 +5,14 @@ module.exports = {
     './index.html'
   ],
   theme: {
+    fontFamily: {
+      sans: ['Inter', 'sans-serif']
+    },
     extend: {
       backgroundImage: {
-        galaxy: "url('/background-galaxy.png')"
+        galaxy: "url('/background-galaxy.png')",
+        'nlw-gradient': 'linear-gradient(89.86deg, #9572fc 20.08%, #43e7ad 55.94%, #e1d55d 90.57%)',
+        'game-gradient': 'linear-gradient(182deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%)',
       }
     },
   },
