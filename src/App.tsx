8488da8dcfe02f70ab2445af895096cf16de4e69
joeyanovich <joedison.dias@gmail.com>
2022-10-29 16:17:28 -0300

    :hammer: Add: component for ad publishing

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { MagnifyingGlassPlus } from 'phosphor-react'
 import { GameBanner } from './components/GameBanner';
+import { CreateAdBanner } from './components/CreateAdBanner';
 
 import './styles/main.css';
 
 import logoImg from './assets/logo-nlw-esports.svg';
+
 
 
 function App() {
@@ -17,20 +19,7 @@ function App() {
       <div className="grid grid-cols-6 gap-6 mt-16">
         <GameBanner bannerUrl="/game-1.png" title="League of Legends" adsCount={5} />
       </div>
-
-      <div className="pt-1 bg-nlw-gradient self-stretch rounded-lg overflow-hidden mt-8">
-        <div className="bg-[#2a2634] px-8 py-6 flex justify-between items-center">
-          <div>
-            <strong className="text-2sxl text-white font-black block">Não encontrou seu duo?</strong>
-            <span className="text-zinc-400 block">Publique um anúncio para encontrar novos players!</span>
-          </div>
-
-          <button className="py-3 px-4 bg-violet-500 hover:bg-violet-600 text-white rounded flex items-center gap-3">
-            <MagnifyingGlassPlus size={24} />
-            Publicar anúncio
-          </button>
-        </div>
-      </div>
+      <CreateAdBanner />
     </div>
   )
 }
